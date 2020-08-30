11519ea6b0f27705ca88084733358a5493426bb6
ke-takashiba <kinmusou33@gmail.com>
2020-08-31 00:38:38 +0900

    j

--- ifbox/settings.py
+++ ifbox/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'user_sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
