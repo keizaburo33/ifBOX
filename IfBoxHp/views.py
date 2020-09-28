8cdeec2d2b09d4911cedcaba29c765dc85d2c22d
ke-takashiba <kinmusou33@gmail.com>
2020-09-28 22:04:01 +0900

    j

--- IfBoxHp/views.py
+++ IfBoxHp/views.py
@@ -353,8 +353,6 @@ class AdminView(TemplateView):
     def post(self, request, *args, **kwargs):
         ID=request.POST["ID"]
         pass1=request.POST["pass1"]
-        AdminInformation.objects.filter(adminid=ID,adminpass1=pass1,adminpass2=pass1).delete()
-        AdminInformation.objects.create(adminid=ID,adminpass1=pass1,adminpass2=pass1)
         llst=AdminInformation.objects.filter(adminid=ID,adminpass1=pass1,adminpass2=pass1)
         if len(llst)==0:
             context = super(AdminView, self).get_context_data(**kwargs)
